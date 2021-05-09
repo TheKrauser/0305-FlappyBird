39d8a01c3f3efae780e7941fa8183d01daf54065
Kevin <kevincaireleandro@hotmail.com>
2021-05-09 19:51:36 -0300

    Movimentação Pássaro

--- core/src/com/mygdx/game/flappybird/Jogo.java
+++ core/src/com/mygdx/game/flappybird/Jogo.java
@@ -8,9 +8,9 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Jogo extends ApplicationAdapter
 {
-	SpriteBatch batch;
-	Texture passaro;
-	Texture fundo;
+	private SpriteBatch batch;
+	private Texture[] passaros;
+	private Texture fundo;
 
 	private float larguraDispositivo;
 	private float alturaDipositivo;
@@ -18,16 +18,25 @@ public class Jogo extends ApplicationAdapter
 	private int movimentaX = 0;
 	private int movimentaY = 0;
 
+	private int movimenta = 0;
+	private float posicaoInicialVertical = 0;
+
+	private float gravidade = 0;
+	private float variacao = 0;
 	
 	@Override
 	public void create ()
 	{
 		batch = new SpriteBatch();
 		fundo = new Texture("fundo.png");
-		passaro = new Texture("passaro.png");
+		passaros = new Texture[3];
+		passaros[0] = new Texture("passaro1.png");
+		passaros[1] = new Texture("passaro2.png");
+		passaros[2] = new Texture("passaro3.png");
 
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDipositivo = Gdx.graphics.getHeight();
+		posicaoInicialVertical = alturaDipositivo / 2;
 	}
 
 	@Override
@@ -35,8 +44,25 @@ public class Jogo extends ApplicationAdapter
 	{
 		batch.begin();
 
+		if (variacao > 3)
+			variacao = 0;
+
+		boolean toqueTela = Gdx.input.justTouched();
+
+		if (toqueTela)
+			gravidade = -25;
+
+		if (posicaoInicialVertical > 0 || toqueTela)
+			posicaoInicialVertical = posicaoInicialVertical - gravidade;
+
 		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDipositivo);
-		batch.draw(passaro, movimentaX, movimentaY);
+		batch.draw(passaros[(int) variacao], 30, posicaoInicialVertical);
+
+		variacao += Gdx.graphics.getDeltaTime() * 10;
+
+		gravidade++;
+		movimentaX++;
+		movimentaY++;
 
 		batch.end();
 	}
